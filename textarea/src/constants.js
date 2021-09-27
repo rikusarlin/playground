e1956f1488ddac3aea093f3a4692bc3d0f4e29ae
s015sir <riku.sarlin@kela.fi>
2021-09-27 16:07:48 +0300

    A slightly more generic example string

--- textarea/src/constants.js
+++ textarea/src/constants.js
@@ -4,7 +4,7 @@ export const NEWLINE_CHAR = '\n';
 export const MAX_NEWLINES = 10;
 export const PARAGRAPH_MARKER = "<p>";
 
-export const textToEdit = 
+export const textToEdit2 = 
 "Enimmäisasumismenot on määrä, joka yleisestä asumistuesta\n"+
 "annetun lain perusteella voidaan ottaa huomioon asumistukea\n"+
 "laskettaessa. Enimmäisasumismenoihin vaikuttavat ruokakunnassa\n"+
@@ -12,3 +12,14 @@ export const textToEdit =
 "\n"+
 "Asumismenoiksi ei asumistuessa hyväksytä esimerkiksi sähkö-,\n"+
 "sauna- ja autopaikkamaksua eikä internetliittymän maksua.";
+
+export const textToEdit =
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n"+
+"sed do eiusmod tempor incididunt ut labore et dolore magna\n"+
+"aliqua. Ut enim ad minim veniam, quis nostrud exercitation\n"+
+"ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis\n"+
+"aute irure dolor in reprehenderit in voluptate velit esse\n"+
+"cillum dolore eu fugiat nulla pariatur.\n"+
+"\n"+
+"Excepteur sint occaecat cupidatat non proident, sunt in culpa\n"+
+"qui officia deserunt mollit anim id est laborum.";
